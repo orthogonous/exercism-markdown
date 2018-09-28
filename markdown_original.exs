d50276eb4579c188e4d3974644971480af1e8de7
orthogonous <orthogonous@gmail.com>
2018-09-28 20:19:57 +1000

    reset original

--- markdown_original.exs
+++ markdown_original.exs
@@ -12,94 +12,10 @@ defmodule Markdown do
   """
   @spec parse(String.t()) :: String.t()
   def parse(m) do
-    process(m)
+    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
   end
 
-  defp process(string) do
-    String.split(string, "\n") |>
-    Enum.map(fn x -> String.graphemes(x) |> process([]) end) |>
-    Enum.join
-  end
-
-  defp process(["_", "_", head | tail], acc) do
-    process(tail, [head, insert(:bold_open) | acc])
-  end
-
-  defp process(["_", "_" | tail], acc) do
-    process(tail, [insert(:bold_close) | acc])
-  end
-
-  defp process(["_", head | tail], acc) do
-    process(tail, [head, insert(:italic_open) | acc], :open)
-  end
-
-  defp process([head, "_" | tail], acc, :open) do
-    process(tail, [insert(:italic_close), head | acc])
-  end
-
-  defp process(["_"], acc) do
-    process([], [insert(:italic_close) | acc])
-  end
-
-  defp process(["#", " " | tail], acc) do
-    process_header(tail, 1, acc)
-  end
-
-  defp process(["#" | tail], acc) do
-    process_header(tail, 1, acc)
-  end
-
-  defp process_header(["#", " " | tail], count, acc) do
-    process_header(tail, count + 1, acc)
-  end
-
-  defp process_header(["#" | tail], count, acc) do
-    process_header(tail, count + 1, acc)
-  end
-
-  defp process_header([head | tail], count, acc) do
-    process_header(tail, count, [head | acc])
-  end
-
-  defp process_header([], count, acc) do
-    header = acc |> Enum.reverse()
-    [insert(:header_open, count), header, insert(:header_close, count)] |> Enum.join()
-  end
-
-
-  # Body of the recursive function
-  defp process([head | tail], acc) do
-    process(tail, [head | acc])
-  end
-
-  defp process([head | tail], acc, :open) do
-    process(tail, [head | acc])
-  end
-
-  # base case for no markup
-  defp process([], acc) do
-    string = acc |> Enum.reverse()
-    [insert(:paragraph_open), string, insert(:paragraph_close)] |> Enum.join()
-  end
-
-  defp insert(:italic_open), do: string2list("<em>")
-  defp insert(:italic_close), do: string2list("</em>")
-  defp insert(:bold_open), do: string2list("<strong>")
-  defp insert(:bold_close), do: string2list("</strong>")
-  defp insert(:header_open, c), do: string2list("<h" <> to_string(c) <> ">")
-  defp insert(:header_close, c), do: string2list("</h" <> to_string(c) <> ">")
-  defp insert(:paragraph_open), do: string2list("<p>")
-  defp insert(:paragraph_close), do: string2list("</p>")
-
-  defp string2list(s), do: String.graphemes(s)
-
-  defp paragraph(s), do: "<p>" <> s <> "</p>"
-
-  defp header(s, size \\ 1) do
-    "<h" <> size <> ">" <> s <> "</h" <> size <> ">"
-  end
-
-  defp processs(t) do
+  defp process(t) do
     if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
       if String.starts_with?(t, "#") do
         enclose_with_header_tag(parse_header_md_level(t))
